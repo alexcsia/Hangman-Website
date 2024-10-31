e6559bc4815d326aa0a7a69b3e09e6ce702929d5
Alexia Bertalan <alexia.bertalan@code.berlin>
2024-10-31 18:07:15 +0100

    add logic for user profile btn

--- app/components/ProfileBtn.tsx
+++ app/components/ProfileBtn.tsx
@@ -1,20 +1,48 @@
 import React from "react";
 import { jwtDecode } from "jwt-decode";
+import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
+import { decode } from "punycode";
 
 interface ProfileBtnProps {
   token: string;
 }
 
+interface DecodedToken {
+  id: string;
+  email: string;
+  username: string;
+}
+
 const ProfileBtn: React.FC<ProfileBtnProps> = ({ token }) => {
-  const handleClick = () => {};
+  const [errorMessage, setErrorMessage] = useState<string | null>("");
+  const router = useRouter();
+
+  const handleClick = () => {
+    try {
+      const decodedToken: DecodedToken = jwtDecode(token);
+      router.push(`/users/profile/${decodedToken.id}`);
+    } catch (error) {
+      console.error("Invalid token:", error);
+      sessionStorage.removeItem("token");
+      setErrorMessage("Please log in first");
+    }
+  };
 
   return (
-    <button
-      className="block mx-4 p-1 font-semibold text-md text-white rounded-md px-6"
-      onClick={handleClick}
-    >
-      Profile
-    </button>
+    <>
+      <button
+        className="block mx-4 p-1 font-semibold text-md text-white rounded-md px-6"
+        onClick={handleClick}
+      >
+        Profile
+      </button>
+      {errorMessage && (
+        <div>
+          <label className="text-red-500">{errorMessage}</label>
+        </div>
+      )}
+    </>
   );
 };
 
