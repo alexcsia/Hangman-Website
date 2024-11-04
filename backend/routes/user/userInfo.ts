dedee9c76b7debd5a4252140f7f70d12724cbaa7
Alexia Bertalan <alexia.bertalan@code.berlin>
2024-11-04 02:19:42 +0100

    userInfo route

--- backend/routes/user/userInfo.ts
+++ backend/routes/user/userInfo.ts
@@ -1,6 +1,6 @@
 import express from "express";
 import { getUserInfo } from "../../modules/user/controllers/userInfo.ts";
-import { authenticateJWT } from "@/backend/loaders/middleware/authenticateJWT.ts";
+import { authenticateJWT } from "../../loaders/middleware/authenticateJWT.ts";
 
 const router = express.Router();
 
