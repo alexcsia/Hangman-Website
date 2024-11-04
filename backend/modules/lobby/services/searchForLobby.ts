94a463621b887bc69393c49d063a2599d3d6879b
Alexia Bertalan <alexia.bertalan@code.berlin>
2024-11-04 15:15:12 +0100

    remove unecessary logging

--- backend/modules/lobby/services/searchForLobby.ts
+++ backend/modules/lobby/services/searchForLobby.ts
@@ -2,10 +2,7 @@ import { Lobby } from "../../models/Lobby.ts";
 
 export const searchForLobby = async (code: string) => {
   try {
-    console.log(code);
-
     const lobby = await Lobby.findOne({ code: code, status: "ongoing" });
-    console.log(lobby);
     return lobby;
   } catch (error: unknown) {
     if (error instanceof Error) throw new Error("Lobby not found");
