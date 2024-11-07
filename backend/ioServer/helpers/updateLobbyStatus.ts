1c9c47338cb0e613bc85c0976391bb66bf7b5fd3
Alexia Bertalan <alexia.bertalan@code.berlin>
2024-11-07 15:43:08 +0100

    create separate ioServer folder

---
+++ backend/ioServer/helpers/updateLobbyStatus.ts
@@ -0,0 +1,12 @@
+import { Lobby } from "../../modules/models/Lobby.ts";
+
+const updateLobbyStatus = async (lobbyId: string) => {
+  const lobby = await Lobby.findById(lobbyId);
+
+  if (lobby) {
+    lobby.status = "ended";
+    await lobby.save();
+  }
+};
+
+export default updateLobbyStatus;
