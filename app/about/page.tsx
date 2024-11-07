480cb9f3b2497018ade577b20e854ce9caa97e01
Alexia Bertalan <alexia.bertalan@code.berlin>
2024-11-07 15:54:54 +0100

    add About page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -2,13 +2,16 @@ import React from "react";
 
 const AboutPage = () => {
   return (
-    <div className="p-4">
-      <h1 className="text-3xl font-bold mb-4">About Us</h1>
-      <p className="text-lg">
-        Welcome to the About page! Here you can learn more about our company,
-        our mission, and our team.
-      </p>
-    </div>
+    <main>
+      <div className="p-4">
+        <h1 className="text-3xl font-bold mb-4">Welcome!</h1>
+        <p className="text-lg">
+          This project allows you to play the classic word-guessing game of
+          Hangman with friends in real time. Create a lobby, share the invite
+          code with a friend and try to guess the word before them!
+        </p>
+      </div>
+    </main>
   );
 };
 
