77f6ebae999bd703e99774f43468e9860ecc787c
Alexia Bertalan <alexia.bertalan@code.berlin>
2024-11-08 03:10:12 +0100

    refactor ioServer

--- backend/ioServer/ioServer.ts
+++ backend/ioServer/ioServer.ts
@@ -17,120 +17,159 @@ interface GameState {
 const lobbies: Record<string, GameState> = {};
 const rematchCounts: Record<string, Set<string>> = {};
 
+const emitGameUpdate = (socket: any, lobbyId: string, playerId: string) => {
+  const lobby = lobbies[lobbyId];
+  const playerState = lobby?.players[playerId];
+  socket.emit("gameUpdate", {
+    word: lobby?.word,
+    wordLength: lobby?.word.length,
+    playerState: playerState,
+  });
+};
+
+const handleJoinLobby = async (
+  socket: any,
+  lobbyId: string,
+  playerId: string,
+  io: Server
+) => {
+  socket.join(lobbyId);
+  console.log(`Player ${playerId} joined lobby: ${lobbyId}`);
+
+  if (!lobbies[lobbyId]) {
+    const randomWord = (await selectRandomWord()) || "example";
+    lobbies[lobbyId] = {
+      word: randomWord,
+      players: {},
+    };
+  }
+
+  if (!lobbies[lobbyId].players[playerId]) {
+    lobbies[lobbyId].players[playerId] = {
+      guessedLetters: [],
+      remainingAttempts: 6,
+    };
+  }
+
+  emitGameUpdate(socket, lobbyId, playerId);
+};
+
+const handleMakeGuess = (
+  socket: any,
+  lobbyId: string,
+  playerId: string,
+  letter: string,
+  username: string,
+  io: Server
+) => {
+  const lobby = lobbies[lobbyId];
+  const playerState = lobby?.players[playerId];
+
+  if (lobby && playerState && playerState.remainingAttempts > 0) {
+    if (!playerState.guessedLetters.includes(letter)) {
+      playerState.guessedLetters.push(letter);
+
+      if (!lobby.word.includes(letter)) {
+        playerState.remainingAttempts--;
+      }
+    }
+
+    emitGameUpdate(socket, lobbyId, playerId);
+
+    const isGameWon = lobby.word
+      .split("")
+      .every((letter) => playerState.guessedLetters.includes(letter));
+
+    if (isGameWon) {
+      io.to(lobbyId).emit("gameOver", `${username} won!`);
+      addWinToUser(playerId).catch(console.log);
+      return;
+    }
+
+    if (playerState.remainingAttempts <= 0) {
+      io.to(lobbyId).emit("gameOver", `${username} is out of tries!`);
+    }
+  }
+};
+
+const handleRematch = async (
+  socket: any,
+  lobbyId: string,
+  playerId: string,
+  io: Server
+) => {
+  if (!rematchCounts[lobbyId]) {
+    rematchCounts[lobbyId] = new Set();
+  }
+
+  rematchCounts[lobbyId].add(playerId);
+
+  if (rematchCounts[lobbyId].size === 2) {
+    const randomWord = (await selectRandomWord()) || "word";
+    lobbies[lobbyId].word = randomWord;
+
+    Object.keys(lobbies[lobbyId].players).forEach((id) => {
+      lobbies[lobbyId].players[id] = {
+        guessedLetters: [],
+        remainingAttempts: 6,
+      };
+    });
+
+    delete rematchCounts[lobbyId];
+
+    io.to(lobbyId).emit("gameUpdate", {
+      word: randomWord,
+      wordLength: randomWord.length,
+      playerState: lobbies[lobbyId].players[playerId],
+    });
+  }
+};
+
+const handleQuit = (
+  socket: any,
+  lobbyId: string,
+  playerId: string,
+  io: Server
+) => {
+  console.log(`User ${playerId} is quitting lobby ${lobbyId}`);
+  updateLobbyStatus(lobbyId);
+  io.to(lobbyId).emit("matchQuit");
+};
+
 export const handleIoEvents = (httpServer: http.Server) => {
   try {
     const io = new Server(httpServer);
 
     io.on("connection", (socket) => {
-      socket.on("joinLobby", async ({ lobbyId, playerId }) => {
-        socket.join(lobbyId);
-        console.log(`Player ${playerId} joined lobby: ${lobbyId}`);
-
-        if (!lobbies[lobbyId]) {
-          const randomWord = (await selectRandomWord()) || "example";
-          lobbies[lobbyId] = {
-            word: randomWord,
-            players: {},
-          };
-        }
-
-        if (!lobbies[lobbyId].players[playerId]) {
-          lobbies[lobbyId].players[playerId] = {
-            guessedLetters: [],
-            remainingAttempts: 6,
-          };
-        }
-
-        // emit the initial game state to each player
-        const playerState = lobbies[lobbyId].players[playerId];
-        console.log(playerState);
-        socket.emit("gameUpdate", {
-          word: lobbies[lobbyId].word,
-          wordLength: lobbies[lobbyId].word.length,
-          playerState: playerState,
-        });
-      });
+      socket.on("joinLobby", async ({ lobbyId, playerId }) =>
+        handleJoinLobby(socket, lobbyId, playerId, io)
+      );
 
       socket.on("chat message", ({ msg, lobbyId }) => {
-        console.log(`message in lobby ${lobbyId} : ${msg} `);
+        console.log(`Message in lobby ${lobbyId}: ${msg}`);
         io.to(lobbyId).emit("chat message", { msg });
       });
 
       socket.on("makeGuess", ({ lobbyId, playerId, letter, username }) => {
-        const lobby = lobbies[lobbyId];
-        const playerState = lobby?.players[playerId];
-
-        if (lobby && playerState && playerState.remainingAttempts > 0) {
-          if (!playerState.guessedLetters.includes(letter)) {
-            playerState.guessedLetters.push(letter);
-
-            if (!lobby.word.includes(letter)) {
-              playerState.remainingAttempts--;
-            }
-          }
-
-          socket.emit("gameUpdate", {
-            word: lobby.word,
-            wordLength: lobby.word.length,
-            playerState: playerState,
-          });
-
-          const isGameWon = lobby.word
-            .split("")
-            .every((letter) => playerState.guessedLetters.includes(letter));
-
-          if (isGameWon) {
-            io.to(lobbyId).emit("gameOver", ` ${username} won!`);
-            addWinToUser(playerId).catch((error) => console.log(error));
-            return;
-          }
-
-          if (playerState.remainingAttempts <= 0) {
-            io.to(lobbyId).emit("gameOver", `${username} is out of tries!`);
-          }
-        }
+        handleMakeGuess(socket, lobbyId, playerId, letter, username, io);
       });
 
       socket.on("rematch", async ({ lobbyId, playerId }) => {
-        if (!rematchCounts[lobbyId]) {
-          rematchCounts[lobbyId] = new Set();
-        }
-
-        rematchCounts[lobbyId].add(playerId);
-
-        if (rematchCounts[lobbyId].size === 2) {
-          const randomWord = (await selectRandomWord()) || "word";
-          lobbies[lobbyId].word = randomWord;
-
-          const playerState = (lobbies[lobbyId].players[playerId] = {
-            guessedLetters: [],
-            remainingAttempts: 6,
-          });
-
-          delete rematchCounts[lobbyId];
-
-          io.to(lobbyId).emit("gameUpdate", {
-            word: randomWord,
-            wordLength: randomWord.length,
-            playerState: playerState,
-          });
-        }
+        handleRematch(socket, lobbyId, playerId, io);
       });
 
       socket.on("quit", ({ lobbyId, playerId }) => {
-        console.log(`User ${playerId} is quitting lobby ${lobbyId}`);
-        updateLobbyStatus(lobbyId);
-        io.to(lobbyId).emit("matchQuit");
+        handleQuit(socket, lobbyId, playerId, io);
       });
 
       socket.on("disconnect", () => {
-        console.log("user disconnected");
+        console.log("User disconnected");
       });
     });
   } catch (error) {
-    if (error instanceof Error) {
-      console.log(error.message);
-      return error.message;
-    }
+    console.error(
+      "Error handling IO events:",
+      error instanceof Error ? error.message : "Unknown error"
+    );
   }
 };
