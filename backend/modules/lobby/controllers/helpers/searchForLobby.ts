8d2a8c975252255ce3517e86801c2338de7ca8dc
Alexia Bertalan <alexia.bertalan@code.berlin>
2024-11-13 03:15:44 +0100

    use ApiError in lobby controller helpers

--- backend/modules/lobby/controllers/helpers/searchForLobby.ts
+++ backend/modules/lobby/controllers/helpers/searchForLobby.ts
@@ -1,10 +1,15 @@
 import { Lobby } from "../../../models/Lobby";
+import { ApiError } from "../../../../errors/ApiError";
 
 export const searchForLobby = async (code: string) => {
   try {
     const lobby = await Lobby.findOne({ code: code, status: "ongoing" });
     return lobby;
   } catch (error: unknown) {
-    if (error instanceof Error) throw new Error("Lobby not found");
+    if (error instanceof Error) {
+      throw new ApiError(404, "Lobby not found");
+    }
+
+    throw new ApiError(500, "An error occurred while searching for the lobby");
   }
 };
