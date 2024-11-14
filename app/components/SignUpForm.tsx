7c3258f9569e47c5f7a622eadb9f11ee13e6398d
Alexia Bertalan <alexia.bertalan@code.berlin>
2024-11-14 20:56:04 +0100

    refactor to use formData object

--- app/components/SignUpForm.tsx
+++ app/components/SignUpForm.tsx
@@ -4,9 +4,11 @@ import { useState, useEffect } from "react";
 import { useRouter } from "next/navigation";
 
 const SignUpForm: React.FC = () => {
-  const [username, setUsername] = useState<string>("");
-  const [email, setEmail] = useState<string>("");
-  const [password, setPassword] = useState<string>("");
+  const [formData, setFormData] = useState({
+    username: "",
+    email: "",
+    password: "",
+  });
   const [errorMessage, setErrorMessage] = useState<string>("");
   const [isSuccessful, setIsSuccessful] = useState<boolean>(false);
   const router = useRouter();
@@ -18,7 +20,7 @@ const SignUpForm: React.FC = () => {
       const response = await fetch("/api/registration/register", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ username, email, password }),
+        body: JSON.stringify(formData),
       });
 
       if (!response.ok) {
@@ -30,9 +32,7 @@ const SignUpForm: React.FC = () => {
         setIsSuccessful(true);
       }
 
-      setUsername("");
-      setEmail("");
-      setPassword("");
+      setFormData({ username: "", email: "", password: "" });
       setErrorMessage("");
     } catch (error: unknown) {
       if (error instanceof Error) {
@@ -45,75 +45,78 @@ const SignUpForm: React.FC = () => {
   useEffect(() => {
     if (isSuccessful) {
       router.push("/");
-      setUsername("");
-      setEmail("");
-      setPassword("");
+      setFormData({ username: "", email: "", password: "" });
       setErrorMessage("");
     }
   }, [isSuccessful, router]);
 
-  return (
-    <>
-      <div className="bg-slate-300 mx-1/2 my-1/2  w-1/3 h-3/4 flex flex-col items-center justify-center rounded">
-        <header className="mb-10 !text-3xl format-header">
-          Create an account
-        </header>
-        <form
-          onSubmit={handleSubmit}
-          className="flex flex-col items-center justify-center mb-10"
-        >
-          <label className="text-left w-full my-2 text-slate-700">
-            Username
-          </label>
-          <input
-            type="text"
-            placeholder="Username"
-            className="border border-gray-300 rounded px-1"
-            onChange={(e) => setUsername(e.target.value)}
-            maxLength={20}
-            required
-          ></input>
-          <label className="text-left w-full my-2 text-slate-700">Email</label>
-          <input
-            type="email"
-            placeholder="Email"
-            className="border border-gray-300 rounded px-1"
-            onChange={(e) => setEmail(e.target.value)}
-            maxLength={254}
-            required
-          ></input>
-          <label className="text-left w-full my-2 text-slate-700">
-            Password
-          </label>
-          <input
-            type="password"
-            placeholder="Password"
-            className="border border-gray-300 rounded px-1"
-            onChange={(e) => setPassword(e.target.value)}
-            maxLength={64}
-            required
-          ></input>
-          <button
-            type="submit"
-            className="my-4 w-auto px-4 font-medium text-xl"
-          >
-            Register
-          </button>
-          {errorMessage && (
-            <div>
-              <label className="text-red-500">{errorMessage}</label>
-            </div>
-          )}
-        </form>
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
 
-        <Link
-          href="/users/login"
-          className="mb-2 text-s underline font-semibold text-slate-700"
-        >
-          Login here
-        </Link>
-      </div>
-    </>
+  return (
+    <div className="bg-slate-300 mx-1/2 my-1/2  w-1/3 h-3/4 flex flex-col items-center justify-center rounded">
+      <header className="mb-10 !text-3xl format-header">
+        Create an account
+      </header>
+      <form
+        onSubmit={handleSubmit}
+        className="flex flex-col items-center justify-center mb-10"
+      >
+        <label className="text-left w-full my-2 text-slate-700">Username</label>
+        <input
+          type="text"
+          name="username"
+          placeholder="Username"
+          className="border border-gray-300 rounded px-1"
+          value={formData.username}
+          onChange={handleChange}
+          maxLength={20}
+          required
+        />
+        <label className="text-left w-full my-2 text-slate-700">Email</label>
+        <input
+          type="email"
+          name="email"
+          placeholder="Email"
+          className="border border-gray-300 rounded px-1"
+          value={formData.email}
+          onChange={handleChange}
+          maxLength={254}
+          required
+        />
+        <label className="text-left w-full my-2 text-slate-700">Password</label>
+        <input
+          type="password"
+          name="password"
+          placeholder="Password"
+          className="border border-gray-300 rounded px-1"
+          value={formData.password}
+          onChange={handleChange}
+          maxLength={64}
+          required
+        />
+        <button type="submit" className="my-4 w-auto px-4 font-medium text-xl">
+          Register
+        </button>
+        {errorMessage && (
+          <div>
+            <label className="text-red-500">{errorMessage}</label>
+          </div>
+        )}
+      </form>
+
+      <Link
+        href="/users/login"
+        className="mb-2 text-s underline font-semibold text-slate-700"
+      >
+        Login here
+      </Link>
+    </div>
   );
 };
 
