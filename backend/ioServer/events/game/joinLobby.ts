cc373c92578a9a420c73d9d9599ed7ad3943503f
Alexia Bertalan <alexia.bertalan@code.berlin>
2024-11-14 21:23:35 +0100

    create intitialize lobby function

--- backend/ioServer/events/game/joinLobby.ts
+++ backend/ioServer/events/game/joinLobby.ts
@@ -9,14 +9,7 @@ export const handleJoinLobby = async (
   io: Server
 ) => {
   addSocketToLobby(socket, lobbyId, playerId);
-
-  if (!lobbies[lobbyId]) {
-    const randomWord = "example";
-    lobbies[lobbyId] = {
-      word: randomWord,
-      players: {},
-    };
-  }
+  initializeLobby(lobbyId);
 
   if (!lobbies[lobbyId].players[playerId]) {
     lobbies[lobbyId].players[playerId] = {
@@ -31,4 +24,14 @@ export const handleJoinLobby = async (
 const addSocketToLobby = (socket: any, lobbyId: string, playerId: string) => {
   socket.join(lobbyId);
   console.log(`Player ${playerId} joined lobby: ${lobbyId}`);
+};
+
+const initializeLobby = (lobbyId: string) => {
+  if (!lobbies[lobbyId]) {
+    const randomWord = "example";
+    lobbies[lobbyId] = {
+      word: randomWord,
+      players: {},
+    };
+  }
 };
